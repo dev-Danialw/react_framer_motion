9b7ed42b3d209d526d95478c43bc0a400dba49f8
Danial-Waheed <daniyalwahied@gmail.com>
2023-03-28 13:52:15 +0500

    init

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="home container">
+      <h2>Welcome to Mouth Melting Pizza</h2>
+      <Link to="/base">
+        <button>Create Your Pizza</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
