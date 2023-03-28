042c3d1ad7671614ac6487f573f17a91cb6e7450
Danial-Waheed <daniyalwahied@gmail.com>
2023-03-28 17:04:07 +0500

    loader with useCycle

---
+++ src/components/Loader.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { motion, useCycle } from "framer-motion";
+
+const loaderVarient = {
+  animationOne: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: {
+      x: {
+        repeatType: "reverse",
+        repeat: Infinity,
+        duration: 0.5,
+      },
+      y: {
+        repeatType: "reverse",
+        repeat: Infinity,
+        duration: 0.25,
+        ease: "easeOut",
+      },
+    },
+  },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      y: {
+        repeatType: "reverse",
+        repeat: Infinity,
+        duration: 0.25,
+        ease: "easeOut",
+      },
+    },
+  },
+};
+
+const Loader = () => {
+  const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
+  return (
+    <>
+      <motion.div
+        className="loader"
+        variants={loaderVarient}
+        animate={animation}
+      ></motion.div>
+      <div onClick={() => cycleAnimation()}>Cycle Loader</div>
+    </>
+  );
+};
+
+export default Loader;
