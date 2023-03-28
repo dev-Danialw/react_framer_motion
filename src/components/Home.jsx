5671eaba1694756ec01a46f752885cffe946d412
Danial-Waheed <daniyalwahied@gmail.com>
2023-03-28 15:13:09 +0500

    keyframes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const buttonVariant = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
@@ -12,13 +20,7 @@ const Home = () => {
     >
       <h2>Welcome to Mouth Melting Pizza</h2>
       <Link to="/base">
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
-        >
+        <motion.button variants={buttonVariant} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
